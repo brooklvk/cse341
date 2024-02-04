8f57aaf27e93bda5865f7330e7e2dd7ce91b93f2
brooklvk <brooklvk@byui.edu>
2024-02-03 19:57:01 -0700

    swagger documentation routes working

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 8080;
+
+const swagger = require('swagger-autogen')();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger_output.json'); 
 
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./database/connection');
 
+const corsOptions = {
+  origin: 'http://localhost:8080', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.static('js'));
 app.use('/', require('./routes'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+const port = process.env.PORT || 8080;
 mongodb.initDb((err, db) => {
     if (err) {
         console.log('Error connecting to the database');
@@ -18,12 +34,29 @@ mongodb.initDb((err, db) => {
       } else {
         console.log('Connected to the database');
         
-        app.listen(port, () => {
-            console.log(`Server running on port ${port}.`)
-        });
+        //app.listen(port) etc 
       }
 });
-
-//call to getDb now, not before initDb^ 
-
 app.get(mongodb);
+
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['./routes/index.js']; // This is the endpoint file
+
+const doc = {
+  info: {
+    version: "1.0.0",
+    title: "Your API Documentation",
+    description: "Description of your API",
+  },
+  host: "localhost:8080", 
+  basePath: "/",
+};
+
+const fs = require('fs');
+if (!fs.existsSync(outputFile)) {
+  swagger(outputFile, endpointsFiles, doc);
+}
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`)
+});
