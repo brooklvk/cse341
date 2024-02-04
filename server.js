dc204d8a9f072df74b0fdde6246586ecc6af033a
brooklvk <brooklvk@byui.edu>
2024-02-03 20:05:16 -0700

    changed host to render from localhost

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./database/connection');
 
 const corsOptions = {
-  origin: 'http://localhost:8080', 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  origin: 'https://cse341-lesson1-75nj.onrender.com', // http://localhost:8080
+  methods: 'GET,PUT,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
@@ -48,7 +48,7 @@ const doc = {
     title: "Your API Documentation",
     description: "Description of your API",
   },
-  host: "localhost:8080", 
+  host: "https://cse341-lesson1-75nj.onrender.com", // "localhost:8080"
   basePath: "/",
 };
 
